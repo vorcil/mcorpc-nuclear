e1bf7b0c977c99f7b69f3bbf57781d4c154e3e68
vorcil <rajol.kochlashvili@medcorp.co.nz>
2015-09-10 22:41:33 +1200

    update to solid-app modules and render of eye sensitivity

--- solid-app.py
+++ solid-app.py
@@ -11,7 +11,35 @@ to delve into the microscopic properties of mostly crystalline solids in raybana
 
 #opposing to the particle physics modules included within the mcorpc repository, i.e. unfocusing on computational analysis of singularities and focus on general summation of "blocks" or "collections" of particles, can average quantities and utilize smaller sized variables (in terms of memory e.g not 10^-30 ~ 100-500)
 
+def motivation():
+    #user interface message
+    technological_motivation="Solid State Physics is right at the heart of the majority of modern society and technology. The entire computer and electronics industry relies on the semiconductor. Solid State Physics is traditionally linked to materials science, chemistry & engineering but also recently overlaps with ibology, biochemistry, biotechnology & medicine."
+
+    #user interface message2
+    fundamental_motivation="The original motivation to study Solid State Physics is that the fundamental physics needed ot understand the microscopic properties of solids is very stimulating. To understand these properties, the ideas and methods of quantum mechanics had to be developed. The physics of solids is very deeply quantum mechanical."
+
+#define the tri circular arrangement
+def bound_SS():
+    #link all three arrangements
+    SS1="Experimental observations and theoretical development"
+    SS2="Fundamental Properties"
+    SS3="Devices, Nano-technology and fabrication"
+
+#define eye sensitivity function
+def eye_sensitivity():
+    #user response
+    m1="Photopic curve, eye sensitivity function, shows the eye's response to different wavelengths of light"
+    m2="The eye's sensitivity is maximum at 555nm (Yellowish-green) and by definition is 683 lm/W"
+    m3="Luminous power (or luminous flux) in lumen represents the pwoer of al imuniuos source (in watt) weighted to match the eye response of the standard observer"
+    #at 555nm
+    1w_555="683.0 LUMENS"
+    #at 480nm
+    1w_480="63.0 LUMENS"
+
+    
+    
 
 
+    
 
-
+    
